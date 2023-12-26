e725f03df08af714f3b5bf660587f8c15370bb80
Manoj Kale <manojkale2000@gmail.com>
2023-12-26 18:46:00 +0530

    modified again

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,8 @@ import { CreateLaptopComponentComponent } from './create-laptop-component/create
 import { UpdateLaptopComponentComponent } from './update-laptop-component/update-laptop-component.component';
 import { LaptopDetailsComponentComponent } from './laptop-details-component/laptop-details-component.component';
 import { ContactUsPageComponent } from './contact-us-page/contact-us-page.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 const routes: Routes = [
   {path: 'laptops', component: LaptopListComponentComponent},
@@ -14,7 +16,9 @@ const routes: Routes = [
   {path: 'update-laptop', component: UpdateLaptopComponentComponent},
   {path: 'laptop-details/:id', component: LaptopDetailsComponentComponent},
   {path: 'laptop-details', component: LaptopDetailsComponentComponent},
-  {path: 'contact-us-page', component: ContactUsPageComponent}
+  {path: 'contact-us-page', component: ContactUsPageComponent},
+  {path: 'sign-in', component: SignInComponent},
+  {path: 'sign-up', component: SignUpComponent}
 ];
 
 @NgModule({
