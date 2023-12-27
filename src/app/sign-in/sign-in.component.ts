7cd74b440db6658ce5489fe4e82d2eeb3b538a35
Manoj Kale <manojkale2000@gmail.com>
2023-12-27 09:58:06 +0530

    Modified th User Interface again for more neat and cleaner look nad feel.

--- src/app/sign-in/sign-in.component.ts
+++ src/app/sign-in/sign-in.component.ts
@@ -1,28 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { Router } from '@angular/router';
 import { UserServiceService } from '../user-service.service';
-import { response } from 'express';
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
-  styleUrl: './sign-in.component.css'
+  styleUrls: ['./sign-in.component.css']
 })
-
 export class SignInComponent {
-  loginStatus: any = sessionStorage.getItem("loginStatus");
+  loginStatus: any;
 
-  constructor(private service: UserServiceService, private router: Router) { }
+  constructor(
+    private service: UserServiceService,
+    private router: Router,
+    @Inject(PLATFORM_ID) private platformId: any
+  ) {
+    // Check if running on the browser before accessing sessionStorage
+    if (isPlatformBrowser(this.platformId)) {
+      this.loginStatus = sessionStorage.getItem("loginStatus");
+    }
+  }
 
   signIN(userEmail: any, userPassword: any) {
     this.service.signIN(userEmail, userPassword).subscribe(response => {
       console.log(response);
       alert('Login Successful');
-      sessionStorage.setItem("loginStatus", "active");
-      sessionStorage.setItem("userId", response.userId);
-      sessionStorage.setItem("userRole", response.userRole);
+      // Check if running on the browser before accessing sessionStorage
+      if (isPlatformBrowser(this.platformId)) {
+        sessionStorage.setItem("loginStatus", "active");
+        sessionStorage.setItem("userId", response.userId);
+        sessionStorage.setItem("userRole", response.userRole);
+      }
       this.goTo();
-
     },
       () => { alert('Wrong Email-ID or Password!'); });
   }
@@ -34,5 +44,4 @@ export class SignInComponent {
   goTo() {
     this.router.navigate(['see-all-laptops']);
   }
-
 }
